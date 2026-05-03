c6280994f8d6130affb2a6d7526fb98af04fbfef
Teja Sri <tejasrigorle2004@gmail.com>
2026-05-03 19:12:14 +0530

    Fix root route for production

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(frontendPath));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(frontendPath, 'index.html'));
   });
 }
